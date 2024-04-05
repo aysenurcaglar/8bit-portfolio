2248f6161f2604b8a62388601f42744721082f08
aysenurcaglar <aysenurcaglar24@gmail.com>
2024-04-05 17:42:54 +0300

    adapted the script

---
+++ src/constants.js
@@ -0,0 +1,24 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I recently embarked on my programming journey, and I've become fairly proficient in JavaScript and React. 
+    Currently, I'm delving into Java, and I'm always excited to learn more. Here is my <a href="https://github.com/aysenurcaglar" target="_blank">Github</a>!`,
+    "cs-degree": `This is my Bachelor's degree, proudly hanging on the wall. 
+    Although it's not directly related to programming, it's a significant part of who I am today, and I am grateful for all the fond memories I made at the school. 
+    You can take a closer look at it <a href="#" target="_blank">here</a>.`,
+    "sofa-table": `Ah, my beloved sofa! When I'm not lost in lines of code, you'll often find me here, indulging in some quality couch potato time. 
+  Sometimes, it's essential to take a break, rest my eyes, and recharge for the next coding adventure.`,
+    tv: `This is my TV. I spend a lot of time on YouTube here, especially watching channels like <a href="https://www.youtube.com/@freecodecamp" target="_blank">freeCodeCamp</a>. 
+  It's actually thanks to their content that I was able to build this cute little website you're exploring. Consider supporting them through donations if you can.`,
+    bed: `As a programmer, getting a good night's sleep is essential for maintaining focus and productivity. 
+  Some of my best ideas come to me when I'm lying here, ready to drift off into dreamland. 
+  It's amazing how a well-rested mind can tackle even the most complex coding challenges with ease.`,
+    resume: `This is my desk and on it is my resume. <a href="#" target="_blank">Check it out!</a>
+  Contact me at example@example.com if you have any interesting job opportunities!`,
+    projects: `Info about this portfolio: It's made using Kaboom.js, which is a library for making games in JavaScript. 
+  The text is rendered with HTML/CSS, so the textbox you're currently reading is not rendered within the canvas. 
+  If you're interested in learning how to use Kaboom.js, I highly recommend checking out <a href="https://youtube.com/@jslegenddev" target="_blank">JSLegendDev</a>'s tutorials.`,
+    library: `These shelves aren't necessarily filled with programming books, as I primarily explore programming concepts through online resources. 
+  However, I just really appreciate the value of literature in helping me stay grounded and broadening my outlook beyond the realm of code.`,
+    exit: `If you'd like to exit this portfolio, just close the tab. Thanks for visiting!`,
+};
